d8d4728a7ab9e73de5c9050cb9403af64420d92b
GreenA89 <alexander.green1989@hotmail.com>
2020-09-30 22:14:01 +0100

    updated compoonents

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,32 +4,47 @@ import Instagram from './instagram.svg';
 import Twitter from './twitter.svg';
 import Pinterest from './pinterest.svg';
 import Facebook from './facebook.svg';
+import menuLinks from '../menu-links/menu-links';
 
 const Footer = () => {
     return (
         <div className='footer'>
             <div className='column border-right'>
-                <div className='column margin-top'>
-                    <span className='font-dark'><p>NEWSLETTER SIGN UP</p></span>
-                    <span className='wide'><input type='email' placeholder='Enter your email' required className='input' ></input><button className='sign-up font-light'>SIGN UP</button></span>
-                    <span className='span'><input type="checkbox"></input><p>Receive news about Poketo Los Angeles?</p></span>
-                </div>
-                <div className='column socials'>
-                    <p className='font-dark padding-bottom'>SOCIAL LINKS</p>
-                    <span className='padding-bottom'>
-                        <button className='social'><img src={Twitter} alt=''></img></button>
-                        <button className='social'><img src={Facebook} alt=''></img></button>
-                        <button className='social'><img src={Pinterest} alt=''></img></button>
-                        <button className='social'><img src={Instagram} alt=''></img></button>
-                    </span>
-                    <p className='font-lighter'>Don't forget to follow us!</p>
-                </div>
-                <div className='copyright column'>
-                    <p>Copyright © 2020 <span className='underline'>Poketo</span></p>
+                <div className='footer-container'>
+                    <div className=''>
+                        <span className='font-dark'><p>NEWSLETTER SIGN UP</p></span>
+                        <span className='wide'><input type='email' placeholder='Enter your email' required className='input' ></input><button className='sign-up font-light'>SIGN UP</button></span>
+                        <span className='span'><input type="checkbox"></input><p>Receive news about Poketo Los Angeles?</p></span>
+                    </div>
+                    <div className='socials'>
+                        <p className='font-dark padding-bottom'>SOCIAL LINKS</p>
+                        <span className='padding-bottom row'>
+                            <a className='social'><img src={Twitter} alt=''></img></a>
+                            <a className='social'><img src={Facebook} alt=''></img></a>
+                            <a className='social'><img src={Pinterest} alt=''></img></a>
+                            <a className='social'><img src={Instagram} alt=''></img></a>
+                        </span>
+                        <p className='font-lighter'>Don't forget to follow us!</p>
+                    </div>
+                    <div className='copyright'>
+                        <p>Copyright © 2020 <span className='underline'>Poketo</span></p>
+                    </div>
                 </div>
             </div>
-            <div className='column'>
-
+            <div className='column padding-left'>
+                <div className='footer-container'>
+                    <div>
+                        <span className='font-dark'><p>WHOLESALE</p></span>
+                        <span className='wholesale'><a className='button underline font-large'>Visit our wholesale site</a></span>
+                        <span className='font-lighter'><p>Are you a retailer interested in carrying Poketo products? We have a site just for you.</p></span>
+                    </div>
+                    <div className='links'>
+                        <span className='font-dark'><p>FOOTER MENU</p></span>
+                        <div className='row'>
+                            {menuLinks.map(link => <a className='link font-lighter' href='#' >{link}</a>)}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
---
+++ src/components/menu-links/menu-links.js
@@ -0,0 +1,18 @@
+const menuLinks = [
+    'About',
+    'Store Locations',
+    'Contact',
+    'Custom Production',
+    'Workshops FAQ',
+    'Returns',
+    'Terms of Service',
+    'Terms of Service',
+    'Journal',
+    'Wholesale',
+    'Shipping FAQ',
+    'Help FAQ',
+    'Careers',
+    'Privacy Policy'
+]
+
+export default menuLinks;
