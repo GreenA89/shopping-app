e6a61fc43a422194d991e5fed648bce58f0ce182
GreenA89 <alexander.green1989@hotmail.com>
2020-10-24 01:01:16 +0100

    completed initial product rendering

--- src/components/tiles-container/tiles-container.jsx
+++ src/components/tiles-container/tiles-container.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import products from '../products/products'
 import './tiles-container.css'
 
 const TilesContainer = () => {
+
+    const [focus, setFocus] = useState(false);
+    const [current, setCurrent] = useState(0);
+
+    const handleFocusTrue = (x, i) => {
+        setFocus(x);
+        setCurrent(i);
+    }
+
+    console.log(focus);
     return (
         <div className='tiles-container'>
-            {products.map(product => 
-                <div className='tile-container'>
+            {products.map((product, i) => 
+                <div className='tile-container' key={i} onMouseEnter={() => handleFocusTrue(true, i)} onMouseLeave={() => handleFocusTrue(false)}>
                     <div className='tile-image'>
-                        <img className='image' src={product.url}></img>
+                        <img className={focus && current === i ? 'image-focus' : 'image'} src={focus && current === i? product.backupurl : product.url}></img>
+                        <button className={focus && current === i ? 'quick-shop' : 'quick-shop-hide'}>QUICK SHOP</button>
                     </div>
-                    <div className='tile-text'>
-                        <p>{product.productname}</p>
-                        <p>${product.price}.00</p>
+                    <div className='tile-text font'>
+                        <p className='product-name'><b>{product.productname}</b></p>
+                        <p className='product-price'><em>${product.price}.00</em></p>
                     </div>
                 </div>)}
         </div>
